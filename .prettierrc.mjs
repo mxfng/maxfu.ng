cd068b5bffeb6a3d15b6326579a3f2b2b18c6626
mxfng <maxhfung@gmail.com>
2026-03-02 09:48:25 -0800

    chore: Additional configs cleanup

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,7 +1,5 @@
 /** @type {import("prettier").Config} */
 export default {
-	printWidth: 100,
-	useTabs: true,
 	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
 	overrides: [
 		{

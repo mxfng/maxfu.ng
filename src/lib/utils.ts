4a910af0c8bfa312ce8f0a5e3f4a0deebc3c3ff8
mxfng <maxhfung@gmail.com>
2026-03-02 09:23:36 -0800

    cleanup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,10 +5,4 @@ function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-function capitalize(str: string): string {
-	return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-const normalizePath = (url: string): string => url.replace(/\/+$/, "");
-
-export { cn, capitalize, normalizePath };
+export { cn };

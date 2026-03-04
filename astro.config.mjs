23081b26b500e9001b3fa5f22e47fa5dbab81aa9
mxfng <maxhfung@gmail.com>
2026-03-04 00:10:49 -0800

    feat: Add rehype-external-links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +15,8 @@ export default defineConfig({
     shikiConfig: {
       theme: "rose-pine-moon",
     },
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] }],
+    ],
   },
 });
